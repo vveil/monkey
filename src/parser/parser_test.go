99b78201273158994a8b390d8df26e4042a90109
Niklas <account@niklas.ai>
2024-07-26 11:29:40 +0200

    added operator test from waaig_code, added testIdentifier function - page 78

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -250,6 +250,126 @@ func TestParsingInfixExpressions(t *testing.T) {
 	}
 }
 
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"-a * b",
+			"((-a) * b)",
+		},
+		// {
+		// 	"!-a",
+		// 	"(!(-a))",
+		// },
+		// {
+		// 	"a + b + c",
+		// 	"((a + b) + c)",
+		// },
+		// {
+		// 	"a + b - c",
+		// 	"((a + b) - c)",
+		// },
+		// {
+		// 	"a * b * c",
+		// 	"((a * b) * c)",
+		// },
+		// {
+		// 	"a * b / c",
+		// 	"((a * b) / c)",
+		// },
+		// {
+		// 	"a + b / c",
+		// 	"(a + (b / c))",
+		// },
+		// {
+		// 	"a + b * c + d / e - f",
+		// 	"(((a + (b * c)) + (d / e)) - f)",
+		// },
+		// {
+		// 	"3 + 4; -5 * 5",
+		// 	"(3 + 4)((-5) * 5)",
+		// },
+		// {
+		// 	"5 > 4 == 3 < 4",
+		// 	"((5 > 4) == (3 < 4))",
+		// },
+		// {
+		// 	"5 < 4 != 3 > 4",
+		// 	"((5 < 4) != (3 > 4))",
+		// },
+		// {
+		// 	"3 + 4 * 5 == 3 * 1 + 4 * 5",
+		// 	"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		// },
+		// {
+		// 	"true",
+		// 	"true",
+		// },
+		// {
+		// 	"false",
+		// 	"false",
+		// },
+		// {
+		// 	"3 > 5 == false",
+		// 	"((3 > 5) == false)",
+		// },
+		// {
+		// 	"3 < 5 == true",
+		// 	"((3 < 5) == true)",
+		// },
+		// {
+		// 	"1 + (2 + 3) + 4",
+		// 	"((1 + (2 + 3)) + 4)",
+		// },
+		// {
+		// 	"(5 + 5) * 2",
+		// 	"((5 + 5) * 2)",
+		// },
+		// {
+		// 	"2 / (5 + 5)",
+		// 	"(2 / (5 + 5))",
+		// },
+		// {
+		// 	"(5 + 5) * 2 * (5 + 5)",
+		// 	"(((5 + 5) * 2) * (5 + 5))",
+		// },
+		// {
+		// 	"-(5 + 5)",
+		// 	"(-(5 + 5))",
+		// },
+		// {
+		// 	"!(true == true)",
+		// 	"(!(true == true))",
+		// },
+		// {
+		// 	"a + add(b * c) + d",
+		// 	"((a + add((b * c))) + d)",
+		// },
+		// {
+		// 	"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+		// 	"add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))",
+		// },
+		// {
+		// 	"add(a + b + c * d / f + g)",
+		// 	"add((((a + b) + ((c * d) / f)) + g))",
+		// },
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		actual := program.String()
+		if actual != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, actual)
+		}
+	}
+}
+
 func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 	integ, ok := il.(*ast.IntegerLiteral)
 	if !ok {
@@ -264,6 +384,26 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 
 	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
 		t.Errorf("integ.TokenLiteral not %d. got=%s", value, integ.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
 		return false
 	}
 
