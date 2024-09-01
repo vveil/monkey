9a4857ab7a703d6828087b2101df83bf1e07c2cb
Niklas <account@niklas.ai>
2024-09-01 10:18:51 +0200

    added builtin len function to get length of strings - page 164

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -1,7 +1,6 @@
 package evaluator
 
 import (
-	"monkey/src/evaluator"
 	"monkey/src/lexer"
 	"monkey/src/object"
 	"monkey/src/parser"
@@ -257,6 +256,39 @@ func TestStringConcatenation(t *testing.T) {
 
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("hello world")`, 11},
+		{`len(1)`, "argument to `len` not supported, got INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Errorf("object is not Error. got=%T (%+v)", evaluated, evaluated)
+				continue
+			}
+
+			if errObj.Message != expected {
+				t.Errorf("wrong error message. expected=%q, got=%q", expected, errObj.Message)
+			}
+		}
 	}
 }
 
